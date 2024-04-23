f8a7819e3c5b2b6705e071b0a667eabc014f982f
tbittner-cc <166669036+tbittner-cc@users.noreply.github.com>
2024-04-23 19:33:17 +0000

    Created more robust search functionality for the site and started refactoring.

--- app.py
+++ app.py
@@ -1,29 +1,54 @@
-from flask import Flask
+from flask import Flask,request,redirect
 from flask import render_template
 
 import spacy
 
+from app import search
+
 app = Flask(__name__)
+
+nlp = spacy.load("en_core_web_sm")
 
 @app.route("/")
 def homepage():
     return render_template('homepage.html')
 
-@app.route("/spacy")
-def spacy_test():
-    nlp = spacy.load("en_core_web_sm")
-    doc = nlp("New York May 7-12")
+@app.route("/search")
+def search():
+    # Get the input text from the form
+    query = request.args.get('query')
+
+    doc = nlp(query)
     
-    location = None
+    location = []
     dates = None
     
     for ent in doc.ents:
-        if ent.label_ == "GPE":  # GPE stands for Geopolitical Entity, which includes countries, cities, etc.
-            location = ent.text
+        if ent.label_ == "GPE": 
+            location.append(ent.text)
         elif ent.label_ == "DATE":
             dates = ent.text
+
+    #If no dates are found, return error
+    if not dates:
+        return "Error: No dates found"
+
+    search.get_dates(dates)
+
+    #If no location is found, return error
+    if len(location) == 0:
+        #return "Error: No location found"   
+        pass
+    #If location contains only one value, go to hotels page
+    elif len(location) == 1:
+        #return redirect("/hotels?location=" + location[0] + "&dates=" + dates)
+        pass
+    #If location contains more than one value, go to flights page
+    elif len(location) > 1:
+        #return redirect("/flights?location=" + "+".join(location))
+        pass
     
     print("Location:", location)
     print("Dates:", dates)
 
-    return "hello nlp"
+    return "nlp"
---
+++ app/search.py
@@ -0,0 +1,15 @@
+import dateutil.parser as parser
+
+def get_dates(dates):
+    (start_date, end_date) = dates.split("-")
+    # Change dates of the form May 1-7 to May 1-May 7
+    # spacy ensures that the dash has no spaces between words
+    if end_date.isdigit():
+        end_date = start_date.split()[0] + " " + end_date
+
+    start_date = parser.parse(start_date)
+    end_date = parser.parse(end_date)
+    delta = end_date - start_date
+    print("Start Date:", start_date.strftime("%B %d, %Y"))
+    print("End Date:", end_date.strftime("%B %d, %Y"))
+    print("Number of days:", delta.days + 1)
