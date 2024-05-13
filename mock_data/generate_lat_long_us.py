06ef97d9bf74eeead5998c09062997b34cef7639
tbittner-cc <166669036+tbittner-cc@users.noreply.github.com>
2024-05-13 15:46:04 +0000

    Generated a list of largest cities in a metro area.

--- mock_data/generate_lat_long_us.py
+++ mock_data/generate_lat_long_us.py
@@ -1,49 +1,43 @@
-import re,sys
+import itertools,json,re,sys
 sys.path.append('../')
 
 import utilities
-
-from geopy.geocoders import Nominatim
 
 # Sample source data
 # {'metro_area': 'New York-Newark-Jersey City, NY-NJ-PA', 'population': '19,620,000', 'city': 'New York', 'state': 'NY', 'country': 'USA'}
 
 # Sample output data
 # Here are the latitude and longitude coordinates for each location:
-# [(34.0522|-118.2437), (40.7128|-74.0060), (41.8781|-87.6298), (30.0521|-90.0716), (30.2672|-97.7431), (38.8950|-77.0365)]
+# [(34.0522|-118.2437), (40.7128|-74.0060), (41.8781|-87.6298), (30.0521|-90.0716), (30.2672|-97.7431)]
 # Let me know if you need anything else!
 
-# Open us_cities.txt and extract the city, state, and country
 with open('us_cities.txt', 'r') as file:
     data = file.read()
+us_cities = json.loads(data)
 
-# Write a regex pattern to extract the city, state, and country from test_data
-location_pattern = r"'city':\s*'(?P<city>[^']+)',\s*'state':\s*'(?P<state>[^']*)',\s*'country':\s*'(?P<country>[^']+)'"
+sub_locations = utilities.build_sublist(us_cities, 10)
 
-# Use the regex pattern to extract the city, state, and country from us_cities.txt
-matches = re.findall(location_pattern, data)
+new_sub_locations = []
 
-locations = [f"{match[0]}, {match[1]}, {match[2]}" for match in matches]
-
-sub_locations = utilities.build_sublist(locations, 10)
-
-#Get the index of the last comma in "Austin, TX, USA" and return everything before it
-
+location_pattern = r'\(\s*(.*?)\s*\|\s*(.*?)\s*\)'
 for sub_loc_list in sub_locations:
-    city_list = "\n".join(sub_loc_list)
+    city_list = "\n".join([f"{city['city']}, {city['state']}, {city['country']}" for city in sub_loc_list])
+
     query = """For the following locations provide latitude and longitude in the following format:
 
-    [(<latitude>,<longitude>),...]
+    [(<latitude>|<longitude>),...]
 
     {}
     """.format(city_list)
-    print(query)
-    print()
 
-# Use Nominatim to get the latitude and longitude of each location
-#geolocator = Nominatim(user_agent="travelectable")
-#geocodes = [geolocator.geocode(location) for location in locations]
-#for idx,geocode in enumerate(geocodes):
-#    print(locations[idx], geocode.address, geocode.latitude, geocode.longitude)
+    data = utilities.execute_llm_query(query)
+    matches = re.findall(location_pattern, data)
+    result = [{'latitude': match[0], 'longitude': match[1]} for match in matches]
 
+    merged_list = [{**sub_loc_list[i], **result[i]} for i in range(len(sub_loc_list))]
+    new_sub_locations.append(merged_list)
 
+us_cities = list(itertools.chain.from_iterable(new_sub_locations))
+
+with open('us_cities.txt', 'w') as output_file:
+    output_file.write(json.dumps(us_cities,indent=4))
