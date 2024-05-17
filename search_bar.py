fc2c3ea4eb4938d7077b4f7d81b60c7bcc4460e5
tbittner-cc <166669036+tbittner-cc@users.noreply.github.com>
2024-05-17 02:16:08 +0000

    Created date parsing logic for the search bar.

---
+++ search_bar.py
@@ -0,0 +1,60 @@
+import re,sqlite3
+
+import spacy
+from spacy.matcher import Matcher
+
+nlp = spacy.load("en_core_web_sm")
+matcher = Matcher(nlp.vocab)
+
+months = ["january", "february", "march", "april", 
+        "may", "june", "july", "august", 
+        "september", "october", "november", "december",
+        "jan", "feb", "mar", "apr", 
+        "may", "jun", "jul", "aug", 
+        "sep", "oct", "nov", "dec"]
+
+date_patterns = [
+    [{"LOWER": {"IN": months}},{"IS_DIGIT": True},{"OP": "?", "LOWER": ","},{"OP": "?", "IS_DIGIT": True}],
+    [{"LOWER": {"IN": months}},{"IS_DIGIT": True},{"OP": "?", "LOWER": ","},{"OP": "?", "IS_DIGIT": True},
+
+    {"OP": "?", "LOWER": "-"},{"OP": "?", "LOWER": "to"},
+
+    {"OP": "?", "LOWER": {"IN": months}},{"IS_DIGIT": True},{"OP": "?", "LOWER": ","},{"OP": "?", "IS_DIGIT": True}]
+]
+
+matcher.add("LOCATION_AND_DATE_PATTERN", date_patterns)
+
+def parse_dates(query):
+    doc = nlp(query)
+    matches = matcher(doc)
+    match_list =[]
+    if matches:
+        for match_id, start, end in matches:
+            #return doc[start:end].text
+            match_list.append(doc[start:end].text)
+        # sort the list by length
+        match_list.sort(key = len, reverse = True)
+        return match_list[0]
+    return None
+
+def is_location_in_database(location):
+    conn = sqlite3.connect('travel_data.db')
+    cur = conn.cursor()
+    cur.execute("SELECT city FROM us_metro_areas")
+    rows = cur.fetchall()
+
+    for row in rows:
+        #Write a regex that matches the city
+        if re.match(location, row[0]):
+            pass
+
+
+
+# Process the text
+if __name__ == '__main__':
+    doc = nlp("may 28 - june 5")
+    matches = matcher(doc)
+    if matches:
+        for match_id, start, end in matches:
+            print("Matched: ", doc[start:end])
+            print("Span: ", doc[start:end].text)
---
+++ tests/tests_search_bar.py
@@ -0,0 +1,29 @@
+import unittest
+
+import search_bar
+
+class TestSearchBar(unittest.TestCase):
+    def test_parse_dates(self):
+        query = "May 28 - Jun 5"
+        result = search_bar.parse_dates(query)
+        self.assertEqual(result, "May 28 - Jun 5")
+
+        query = "September 1 to 7"
+        result = search_bar.parse_dates(query)
+        self.assertEqual(result, "September 1 to 7")
+
+        query = "Aug 6"
+        result = search_bar.parse_dates(query)
+        self.assertEqual(result, "Aug 6")
+
+        query = "October 6, 2023"
+        result = search_bar.parse_dates(query)
+        self.assertEqual(result, "October 6, 2023")
+
+        query = "Nov 7, 2023 - December 4, 2023"
+        result = search_bar.parse_dates(query)
+        self.assertEqual(result, "Nov 7, 2023 - December 4, 2023")
+
+        query = "Chicago to Austin"
+        result = search_bar.parse_dates(query)
+        self.assertEqual(result, None)
