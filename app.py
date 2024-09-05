05e7189fc4fe16d3a1b4f5bf3355a575ebb5fba5
Coastal Chicago <166669036+tbittner-cc@users.noreply.github.com>
2024-09-05 19:39:50 +0000

    Altered image retrieval to reduce the number of GET requests

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from datetime import datetime
-from flask import Flask,make_response,request,redirect,render_template,session
+from flask import Flask, make_response, request, redirect, render_template, session
 import spacy
-import sqlite3
 import utilities
 
 app = Flask(__name__)
@@ -11,34 +10,44 @@ nlp = spacy.load("en_core_web_sm")
 
 locations = utilities.get_all_locations()
 
+
 @app.route("/")
 def homepage():
     session.clear()
-    return render_template('homepage.html',
-                           locations = [f"{location[1]}, {location[2]}" for location in locations],
-                           suggested_date_range = utilities.get_suggested_dates(datetime.now()))
+    return render_template(
+        'homepage.html',
+        locations=[f"{location[1]}, {location[2]}" for location in locations],
+        suggested_date_range=utilities.get_suggested_dates(datetime.now()))
 
-def add_lead_rates(hotels,dates):
-    lead_rates = utilities.get_lead_rates(hotels,session['dates'][0])
-    for (hotel_id,rate) in lead_rates:
+
+def add_lead_rates(hotels, dates):
+    lead_rates = utilities.get_lead_rates(hotels, session['dates'][0])
+    for (hotel_id, rate) in lead_rates:
         for hotel in hotels:
             if hotel['id'] == hotel_id:
                 hotel['lead_rate'] = rate
+
 
 @app.route("/hotels")
 def hotels():
     session['filtered_amenities'] = []
     hotels = utilities.get_hotels(session['destination'])
-    add_lead_rates(hotels,session['dates'])
-    
-    return render_template('hotel_search_results.html',hotel_location = session['destination'][1],
-                           hotels = hotels,start_date = session['dates'][0],end_date = session['dates'][1],
-                           amenities = utilities.get_amenities(session['destination'][0]))
+    add_lead_rates(hotels, session['dates'])
 
-@app.route("/hotel-sort", methods=['GET','POST'])
+    return render_template('hotel_search_results.html',
+                           hotel_location=session['destination'][1],
+                           hotels=hotels,
+                           start_date=session['dates'][0],
+                           end_date=session['dates'][1],
+                           amenities=utilities.get_amenities(
+                               session['destination'][0]))
+
+
+@app.route("/hotel-sort", methods=['GET', 'POST'])
 def hotel_sort():
-    hotels = utilities.get_hotels_with_amenities(session['destination'],session['filtered_amenities'])
-    add_lead_rates(hotels,session['dates'])
+    hotels = utilities.get_hotels_with_amenities(session['destination'],
+                                                 session['filtered_amenities'])
+    add_lead_rates(hotels, session['dates'])
 
     sort_option = request.form['sort-hotels-by']
     if sort_option == '':
@@ -50,66 +59,88 @@ def hotel_sort():
     elif sort_option == 'rating-high':
         hotels = sorted(hotels, key=lambda x: x['star_rating'], reverse=True)
 
-    template =render_template('hotel_search_card.html',
-                        hotel_location = session['destination'][1],
-                        hotels = hotels,start_date = session['dates'][0],end_date = session['dates'][1])
+    template = render_template('hotel_search_card.html',
+                               hotel_location=session['destination'][1],
+                               hotels=hotels,
+                               start_date=session['dates'][0],
+                               end_date=session['dates'][1])
     return template
 
-@app.route("/add-amenity", methods=['GET','POST'])
+
+@app.route("/add-amenity", methods=['GET', 'POST'])
 def add_amenity():
     all_utilities = utilities.get_amenities(session['destination'][0])
     fa = session['filtered_amenities']
     # Don't add blanks or incomplete values like "W" for "Wifi"
-    if request.form['amenities_input'] != '' and request.form['amenities_input'] in all_utilities:
+    if request.form['amenities_input'] != '' and request.form[
+            'amenities_input'] in all_utilities:
         fa.append(request.form['amenities_input'])
         session['filtered_amenities'] = fa
-    resp = make_response(render_template('filtered_amenities.html',filtered_amenities = session['filtered_amenities'],
-        amenities = all_utilities))
+    resp = make_response(
+        render_template('filtered_amenities.html',
+                        filtered_amenities=session['filtered_amenities'],
+                        amenities=all_utilities))
     resp.headers.set('HX-Trigger', 'updateFilteredAmenities')
 
     return resp
 
-@app.route("/remove-amenity", methods=['GET','POST'])
+
+@app.route("/remove-amenity", methods=['GET', 'POST'])
 def remove_amenity():
     fa = session['filtered_amenities']
     # We know we'll only have one key in the form and that maps to our unique amenity value
     value = list(request.form.to_dict().values())[0]
     fa.remove(value)
     session['filtered_amenities'] = fa
-    resp = make_response(render_template('filtered_amenities.html',filtered_amenities = session['filtered_amenities'],
-        amenities = utilities.get_amenities(session['destination'][0])))
+    resp = make_response(
+        render_template('filtered_amenities.html',
+                        filtered_amenities=session['filtered_amenities'],
+                        amenities=utilities.get_amenities(
+                            session['destination'][0])))
     resp.headers.set('HX-Trigger', 'updateFilteredAmenities')
 
     return resp
 
-@app.route("/hotel-amenity-results", methods=['GET','POST'])
-def hotel_amenity_results():
-    hotels = utilities.get_hotels_with_amenities(session['destination'],session['filtered_amenities'])
-    add_lead_rates(hotels,session['dates'])
 
-    template =render_template('hotel_search_card.html',
-                        hotel_location = session['destination'][1],
-                        hotels = hotels,start_date = session['dates'][0],end_date = session['dates'][1])
+@app.route("/hotel-amenity-results", methods=['GET', 'POST'])
+def hotel_amenity_results():
+    hotels = utilities.get_hotels_with_amenities(session['destination'],
+                                                 session['filtered_amenities'])
+    add_lead_rates(hotels, session['dates'])
+
+    template = render_template('hotel_search_card.html',
+                               hotel_location=session['destination'][1],
+                               hotels=hotels,
+                               start_date=session['dates'][0],
+                               end_date=session['dates'][1])
     return template
 
-@app.route("/hotel-details", methods=['GET','POST'])
+
+@app.route("/hotel-details", methods=['GET', 'POST'])
 def hotel_details():
     hotel_id = request.form['hotel_id']
     is_winter_rate = utilities.is_winter_rate(session['dates'][0])
-    hotel = utilities.get_hotel_details(session['destination'],session['dates'],hotel_id, is_winter_rate)
-    return render_template('hotel_details.html',hotel_location = session['destination'],
-                           hotel = hotel)
+    hotel = utilities.get_hotel_details(session['destination'],
+                                        session['dates'], hotel_id,
+                                        is_winter_rate)
+    return render_template('hotel_details.html',
+                           hotel_location=session['destination'],
+                           hotel=hotel)
 
-@app.route("/search", methods=['GET','POST'])
+
+@app.route("/search", methods=['GET', 'POST'])
 def search():
-    location_queries = {'origin': request.form['origin'], 'destination': request.form['destination']}
+    location_queries = {
+        'origin': request.form['origin'],
+        'destination': request.form['destination']
+    }
     date_range = request.form['date_range']
 
     doc = nlp(date_range)
-    
+
     date_string = None
     error = None
-    
+
     for ent in doc.ents:
         if ent.label_ == "DATE":
             date_string = ent.text
@@ -120,13 +151,17 @@ def search():
     dates = utilities.parse_dates(date_string)
     session['dates'] = dates
 
-    selected_locations = utilities.get_selected_locations(location_queries,locations)
+    selected_locations = utilities.get_selected_locations(
+        location_queries, locations)
 
-    if location_queries['origin'] == '' and location_queries['destination'] == '':
+    if location_queries['origin'] == '' and location_queries[
+            'destination'] == '':
         error = "No locations found"
-        return render_template('homepage.html', locations = [location[1] for location in locations],
-                           suggested_date_range = utilities.get_suggested_dates(datetime.now()),
-                           error=error)
+        return render_template(
+            'homepage.html',
+            locations=[location[1] for location in locations],
+            suggested_date_range=utilities.get_suggested_dates(datetime.now()),
+            error=error)
     # If either origin or destination is empty take the other
     elif location_queries['origin'] == '':
         session['destination'] = selected_locations[1]
@@ -140,11 +175,13 @@ def search():
         return redirect("/hotels")
     return "nlp"
 
-@app.route("/checkout", methods=['GET','POST'])
+
+@app.route("/checkout", methods=['GET', 'POST'])
 def checkout():
     return render_template('checkout.html')
 
-@app.route("/complete-booking", methods=['GET','POST'])
+
+@app.route("/complete-booking", methods=['GET', 'POST'])
 def complete_booking():
     action = request.form['action']
 
--- utilities.py
+++ utilities.py
@@ -1,12 +1,15 @@
 import ast
+import base64
 from datetime import datetime, timedelta
 import dateutil.parser as parser
 import random
 import sqlite3
 
+
 def is_winter_rate(date):
     month = date.month
     return month in [11, 12, 1, 2, 3, 4]
+
 
 def parse_dates(dates):
     (start_date, end_date) = dates.split("-")
@@ -21,17 +24,23 @@ def parse_dates(dates):
 
     return (start_date, end_date)
 
+
 # Suggest a date range two weeks in the future for searching.
 def get_suggested_dates(current_date):
     suggested_start_date = current_date + timedelta(weeks=2)
-    suggested_end_date = suggested_start_date + timedelta(days = 6)
+    suggested_end_date = suggested_start_date + timedelta(days=6)
 
     if (suggested_start_date.month == suggested_end_date.month):
-        suggested_date_range = "{}-{}".format(suggested_start_date.strftime("%B %-d"), suggested_end_date.strftime("%-d"))
+        suggested_date_range = "{}-{}".format(
+            suggested_start_date.strftime("%B %-d"),
+            suggested_end_date.strftime("%-d"))
     else:
-        suggested_date_range = "{}-{}".format(suggested_start_date.strftime("%B %-d"), suggested_end_date.strftime("%B %-d"))
+        suggested_date_range = "{}-{}".format(
+            suggested_start_date.strftime("%B %-d"),
+            suggested_end_date.strftime("%B %-d"))
 
     return suggested_date_range
+
 
 def get_all_locations():
     locations = []
@@ -40,30 +49,35 @@ def get_all_locations():
         curr.execute("SELECT id,location,country FROM destinations")
         rows = curr.fetchall()
         for row in rows:
-                locations.append((row[0],row[1],row[2]))
+            locations.append((row[0], row[1], row[2]))
 
     return locations
+
 
 def get_hotels(location):
     with sqlite3.connect("travelectable.db") as conn:
         curr = conn.cursor()
-        curr.execute("SELECT id,name,address,distance,star_rating,description FROM hotels WHERE location_id = ?",
-                     (location[0],))
+        curr.execute(
+            "SELECT id,name,address,distance,star_rating,description FROM hotels WHERE location_id = ?",
+            (location[0], ))
         rows = curr.fetchall()
         columns = [column[0] for column in curr.description]
         hotels = [dict(zip(columns, row)) for row in rows]
-    
+
         for hotel in hotels:
             image_name = return_hotel_image_path(hotel['name'])
             location_name = return_location_image_path(location[1])
-            hotel['image'] = image_name
             hotel['location'] = location_name
+            with open(f"static/images/{location_name}/{image_name}/ext.png",
+                      "rb") as f:
+                hotel['image'] = base64.b64encode(f.read()).decode()
 
-            curr.execute("""
+            curr.execute(
+                """
                 SELECT distinct a.amenity FROM amenities a 
                 JOIN room_rates_amenities_xref xref ON a.id = xref.amenity_id
                 JOIN room_rates rr ON xref.room_rate_id = rr.id
-                WHERE rr.hotel_id = ?""", (hotel['id'],))
+                WHERE rr.hotel_id = ?""", (hotel['id'], ))
 
             rows = curr.fetchall()
             hotel['amenities'] = set([row[0] for row in rows])
@@ -72,52 +86,58 @@ def get_hotels(location):
 
     return hotels
 
+
 def get_amenities(location_id):
     with sqlite3.connect("travelectable.db") as conn:
         curr = conn.cursor()
-        curr.execute("""SELECT amenity FROM amenities a JOIN room_rates_amenities_xref xref ON a.id = xref.amenity_id 
+        curr.execute(
+            """SELECT amenity FROM amenities a JOIN room_rates_amenities_xref xref ON a.id = xref.amenity_id 
                         JOIN room_rates rr ON xref.room_rate_id = rr.id JOIN hotels h ON rr.hotel_id = h.id 
-                        WHERE h.location_id = ?""",
-                     (location_id,))
+                        WHERE h.location_id = ?""", (location_id, ))
         rows = curr.fetchall()
 
     amenities = set()
 
     for row in rows:
-        amenities.add(row[0])    
+        amenities.add(row[0])
 
     return sorted(list(amenities))
 
-def get_hotels_with_amenities(location,amenities):
+
+def get_hotels_with_amenities(location, amenities):
     all_hotels = get_hotels(location)
-    
+
     if len(amenities) == 0:
         # If nothing's filtered, return all hotels
         return all_hotels
 
     amenities_set = set(amenities)
-    return [hotel for hotel in all_hotels if amenities_set.issubset(hotel['amenities'])]
-    
-def get_lead_rates(hotels,date):
+    return [
+        hotel for hotel in all_hotels
+        if amenities_set.issubset(hotel['amenities'])
+    ]
+
+
+def get_lead_rates(hotels, date):
     hotel_ids = [hotel['id'] for hotel in hotels]
     with sqlite3.connect("travelectable.db") as conn:
         curr = conn.cursor()
         query = "SELECT hotel_id,winter_rate,summer_rate FROM room_rates WHERE hotel_id IN ({})"\
             .format(",".join(['?' for _ in hotel_ids]))
-        curr.execute(query,hotel_ids)
+        curr.execute(query, hotel_ids)
         rows = curr.fetchall()
 
     if is_winter_rate(date):
-        all_rates = [(row[0],row[1]) for row in rows]
+        all_rates = [(row[0], row[1]) for row in rows]
     else:
-        all_rates = [(row[0],row[2]) for row in rows]
+        all_rates = [(row[0], row[2]) for row in rows]
 
-    all_rates = sorted(all_rates, key=lambda x: (x[0],int(x[1])))
+    all_rates = sorted(all_rates, key=lambda x: (x[0], int(x[1])))
 
     # Return the lowest rate for each hotel
     lead_rate_dict = {}
     for rate in all_rates:
-        first,second = rate 
+        first, second = rate
         if first not in lead_rate_dict:
             lead_rate_dict[first] = second
 
@@ -127,12 +147,14 @@ def get_lead_rates(hotels,date):
 
     return list(lead_rate_dict.items())
 
-def get_hotel_details(location,dates,hotel_id,is_winter_rate):
-    duration = (dates[1]-dates[0]).days
+
+def get_hotel_details(location, dates, hotel_id, is_winter_rate):
+    duration = (dates[1] - dates[0]).days
     with sqlite3.connect("travelectable.db") as conn:
         curr = conn.cursor()
-        curr.execute("""SELECT id,room_type,room_description,winter_rate,summer_rate,amenities,cancellation_policy 
-            FROM room_rates WHERE hotel_id = ?""",(hotel_id,))
+        curr.execute(
+            """SELECT id,room_type,room_description,winter_rate,summer_rate,amenities,cancellation_policy 
+            FROM room_rates WHERE hotel_id = ?""", (hotel_id, ))
         rows = curr.fetchall()
         columns = [column[0] for column in curr.description]
         rates = [dict(zip(columns, row)) for row in rows]
@@ -141,12 +163,15 @@ def get_hotel_details(location,dates,hotel_id,is_winter_rate):
         rate['is_winter_rate'] = is_winter_rate
         amenities = ast.literal_eval(rate['amenities'])
         rate['amenities'] = amenities
-        rate['image'] = return_room_rate_image_path(rate['room_type'])
-        rate['winter_total'] = str(int(rate['winter_rate'])*int(duration))
-        rate['summer_total'] = str(int(rate['summer_rate'])*int(duration))
+        image = return_room_rate_image_path(rate['room_type'])
+        with open(f"static/images/room_rates/{image}.png", "rb") as f:
+            rate['image'] = base64.b64encode(f.read()).decode()
+        rate['winter_total'] = str(int(rate['winter_rate']) * int(duration))
+        rate['summer_total'] = str(int(rate['summer_rate']) * int(duration))
 
-    curr.execute("SELECT id,name,address,distance,star_rating,description FROM hotels WHERE id = ?",
-                     (hotel_id,))
+    curr.execute(
+        "SELECT id,name,address,distance,star_rating,description FROM hotels WHERE id = ?",
+        (hotel_id, ))
     row = curr.fetchone()
     columns = [column[0] for column in curr.description]
     hotel = dict(zip(columns, row))
@@ -155,32 +180,48 @@ def get_hotel_details(location,dates,hotel_id,is_winter_rate):
 
     image_name = return_hotel_image_path(hotel['name'])
     location_name = return_location_image_path(location[1])
-    hotel['image'] = image_name
+    with open(f"static/images/{location_name}/{image_name}/ext.png",
+              "rb") as f:
+        hotel['ext_image'] = base64.b64encode(f.read()).decode()
+    with open(f"static/images/{location_name}/{image_name}/int.png",
+              "rb") as f:
+        hotel['int_image'] = base64.b64encode(f.read()).decode()
     hotel['location'] = location_name
     hotel['dates'] = dates
 
     return hotel
 
-def get_selected_locations(location_queries,locations):
+
+def get_selected_locations(location_queries, locations):
     selected_locations = []
-    location_query_tuple = (location_queries['origin'],location_queries['destination'])
+    location_query_tuple = (location_queries['origin'],
+                            location_queries['destination'])
     for i in location_query_tuple:
         if i == '':
             selected_locations.append(i)
         else:
-            selected_location = [location for location in locations if f"{location[1]}, {location[2]}" == i][0]
+            selected_location = [
+                location for location in locations
+                if f"{location[1]}, {location[2]}" == i
+            ][0]
             selected_locations.append(selected_location)
 
     return selected_locations
 
+
 def return_location_image_path(location_name):
-    file_path = location_name.replace(' ','_').replace('.','').lower()
+    file_path = location_name.replace(' ', '_').replace('.', '').lower()
     return file_path
+
 
 def return_hotel_image_path(hotel_name):
-    file_path = hotel_name.replace(' ','_').replace('.','').replace('\'','').lower()
+    file_path = hotel_name.replace(' ', '_').replace('.',
+                                                     '').replace('\'',
+                                                                 '').lower()
     return file_path
 
+
 def return_room_rate_image_path(room_type):
-    file_path = room_type.replace(' ','_').replace('.','').replace('\'','').replace('/','-').lower()
+    file_path = room_type.replace(' ', '_').replace('.', '').replace(
+        '\'', '').replace('/', '-').lower()
     return file_path
