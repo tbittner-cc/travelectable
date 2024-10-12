242fa5f22949cb6f741e9d9fb1907ca249f2fbdd
Todd Bittner <tbittner@coastalchicago.com>
2024-10-12 14:55:36 -0500

    Building out filter functionality

--- flight_utilities.py
+++ flight_utilities.py
@@ -1,5 +1,5 @@
 import base64
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
 import pytz
 import sqlite3
 
@@ -10,18 +10,30 @@ def retrieve_airports(location):
 
 
 def get_flight_duration(flight_date, dep_time, arr_time, dep_tz, arr_tz):
-    # It's possible on some very long-haul flights that the arrival time is 
-    # slightly after the departure time, but we'll cross that bridge when 
+    # It's possible on some very long-haul flights that the arrival time is
+    # slightly after the departure time, but we'll cross that bridge when
     # we get to it.
     if arr_time < dep_time:
         arrival_date = flight_date + timedelta(days=1)
     else:
         arrival_date = flight_date
 
-    dep_time = datetime(flight_date.year, flight_date.month, flight_date.day, dep_time.hour, dep_time.minute)
+    dep_time = datetime(
+        flight_date.year,
+        flight_date.month,
+        flight_date.day,
+        dep_time.hour,
+        dep_time.minute,
+    )
     dep_time = dep_tz.localize(dep_time)
 
-    arr_time = datetime(arrival_date.year, arrival_date.month, arrival_date.day, arr_time.hour, arr_time.minute)
+    arr_time = datetime(
+        arrival_date.year,
+        arrival_date.month,
+        arrival_date.day,
+        arr_time.hour,
+        arr_time.minute,
+    )
     arr_time = arr_tz.localize(arr_time)
 
     time_delta = arr_time - dep_time
@@ -68,10 +80,13 @@ def get_flight_search_results(origin, destination, flight_date):
                 for flight_option in flight_options:
                     merged_option = {**flight, **flight_option}
 
-                    airline_icon_name = merged_option['airline'].replace(' ', '_').lower()
-                    with open(f"static/airline_icons/{airline_icon_name}.webp",
-                      "rb") as f:
-                        merged_option['logo'] = base64.b64encode(f.read()).decode()
+                    airline_icon_name = (
+                        merged_option["airline"].replace(" ", "_").lower()
+                    )
+                    with open(
+                        f"static/airline_icons/{airline_icon_name}.webp", "rb"
+                    ) as f:
+                        merged_option["logo"] = base64.b64encode(f.read()).decode()
 
                     merged_option["departure_time"] = datetime.strptime(
                         merged_option["departure_time"], "%H:%M"
@@ -92,3 +107,29 @@ def get_flight_search_results(origin, destination, flight_date):
                     search_results.append(merged_option)
 
     return search_results
+
+
+def generate_filters(flight_search_results):
+    flight_filters = {
+        "stops": {},
+        "airlines": {},
+        "layover_airports": {},
+    }
+
+    for result in flight_search_results:
+        flight_filters["stops"][result["num_stops"]] = (
+            flight_filters["stops"].get(result["num_stops"], 0) + 1
+        )
+
+        flight_filters["airlines"][result["airline"]] = (
+            flight_filters["airlines"].get(result["airline"], 0) + 1
+        )
+
+        if result["layover_airports"] != "":
+            layover_airports = [x.strip() for x in result["layover_airports"].strip('(').strip(')').split(",")]
+            for layover_airport in layover_airports:
+                flight_filters["layover_airports"][layover_airport] = (
+                    flight_filters["layover_airports"].get(layover_airport, 0) + 1
+                )
+                
+    return flight_filters
